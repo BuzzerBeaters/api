b16dbbde22b4c58cc592fd29ac857ec2089491bf
fabien zibi <fabien.zibi@gmail.com>
2020-01-30 23:01:46 +0100

    feat: get data

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 const express = require('express')
-const app = express()
+const app = express();
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = 'https://www.basketball-reference.com/boxscores/';
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('It works \o/'))
+const fetchData = async () => {
+  const result = await axios.get(url);
+  return cheerio.load(result.data);
+};
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+
+
+const yolo = async () => {
+  const $ = await fetchData();
+  const data = [];
+  $('.teams').each((_, element) => {
+    const obj = {}
+    obj.loser = $(element).find('.loser td a').text().replace('Final', '')
+    obj.winner = $(element).find('.winner td a').text().replace('Final', '')
+
+    const score1 = $(element).find('.loser .right').first().text()
+    const score2 = $(element).find('.winner .right').first().text()
+
+    obj.delta = Math.abs(parseInt(score1, 10) - parseInt(score2));
+
+    data.push(obj);
+  });
+
+  return data;
+}
+
+app.get('/', async (_, res) => {
+  const data = await yolo();
+  console.log({data})
+  res.send(data)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
