5bf1ec15bea0e845add7238757d42575aa900cf5
fabien zibi <fabien.zibi@gmail.com>
2020-02-01 14:44:48 +0100

    feat: first ui

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const url = 'https://www.basketball-reference.com/boxscores/';
 
-// app.use(express.json());
-
 const port = process.env.PORT || 3000;
 
 const fetchData = async () => {
@@ -16,7 +14,7 @@ const fetchData = async () => {
 
 
 
-const yolo = async () => {
+const getGamesData = async () => {
   const $ = await fetchData();
   const data = [];
   $('.teams').each((_, element) => {
@@ -32,27 +30,76 @@ const yolo = async () => {
     data.push(obj);
   });
 
-  return data;
+  return data.sort((a, b) => a.delta > b.delta);
 }
 
-app.get('/games', async (_, res) => {
-  // const data = await yolo();
-  const html = `<!DOCTYPE html>
+const renderGames = (data) => {
+  const randomizr = Math.random() < 0.5;
+  return `<li class='game'>
+    <span class='team'>${randomizr ? data.winner.substr(0, 3) : data.loser.substr(0, 3)}</span>
+    <span class='vs'>vs</span>
+    <span class='team'>${randomizr ? data.loser.substr(0, 3) : data.winner.substr(0, 3)}</span>
+  </li>`
+}
+
+app.get('/', async (_, res) => {
+  const games = await getGamesData();
+  let html = `<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Buzzer Beater</title>
+    <title>SAMPLE</title>
+    <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet"> 
+    <style>
+      body {
+        background: black;
+        color: white;
+        margin: 0;
+        padding:0;
+      }
+      .games {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+      }
+      .game {
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        line-height: 1;
+        font-family: "Bangers";
+      }
+      .team {
+        font-size: 5rem;
+        line-height: 1;
+      }
+      .vs {
+        font-size: 3.5rem;
+        padding: 0 2rem;
+      }
+    </style>
   </head>
   <body>
-    HELLO
-  </body>
-  </html>`
+    <ul class="games">`;
+
+  games.map(game => {
+    html += renderGames(game)
+  })
+
+  html += '</ul></body></html>';
   res.send(html)
 })
 
-app.get('/', (_, res) => res.send('It works \o/'))
+// app.get('/', (_, res) => res.send('It works \o/'))
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
 
