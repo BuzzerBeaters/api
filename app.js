d97c1caa7bce40226c12f269146b961a5146db5f
fabien zibi <fabien.zibi@gmail.com>
2020-03-20 10:52:21 +0100

    fix: title

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', async (_, res) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SAMPLE</title>
+    <title>I want basketball</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"> 
     <style>
       body {
